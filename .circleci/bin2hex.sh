fd7a67fa9995fffc8689b4147c957655fa2e3b55
kenherring <kennethherring@gmail.com>
2023-09-18 06:44:05 -0400

    circleci secret progress key

---
+++ .circleci/bin2hex.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+(
+  export LC_ALL=C
+    od -An -vtx1 |
+    tr -s ' \t\n' '\n\n\n' |
+    grep .
+)
--- .docker/docker-build.sh
+++ .docker/docker-build.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 set -eou pipefail
 
-if [ ! -f secrets/progress.cfg ]; then
-  PROGRESS_CFG=$DLC/progress.cfg
-  [ ! -f "$PROGRESS_CFG" ] && PROGRESS_CFG=/c/Progress/OpenEdge/progress.cfg
-  if [ ! -f "$PROGRESS_CFG" ]; then
-    echo "ERROR: cannot find progress.cfg"
-    exit 1
+main_block () {
+  get_progress_cfg
+  build_docker_image
+  echo "docker build successful!"
+  exit 0
+}
+
+get_progress_cfg () {
+  [ -f secrets/progress.cfg ] || return 0
+
+  if ${CIRCLECI:-false}; then
+    PROGRESS_CFG_HEX="$(.circleci/bin2hex.sh < secrets/progress.cfg)"
+  else
+    PROGRESS_CFG=$DLC/progress.cfg
+    [ ! -f "$PROGRESS_CFG" ] && PROGRESS_CFG=/c/Progress/OpenEdge/progress.cfg
+    if [ ! -f "$PROGRESS_CFG" ]; then
+      echo "ERROR: cannot find progress.cfg"
+      exit 1
+    fi
+    PROGRESS_CFG_HEX="$(.circleci/bin2hex.sh < "$PROGRESS_CFG")"
   fi
+
   mkdir -p secrets
-  cp "$PROGRESS_CFG" secrets/progress.cfg
-fi
+  .circleci/hex2bin.sh <<< "$(tr ' ' '\n' <<< "$PROGRESS_CFG_HEX")" > secrets/progress.cfg
+}
 
-docker build -f .docker/Dockerfile --secret id=PROGRESS_CFG,src="$DLC/progress.cfg" . --tag oedb:latest
-docker tag oedb:latest kherring/oedb:latest
+build_docker_image () {
+  docker build -f .docker/Dockerfile --secret id=PROGRESS_CFG,src="$DLC/progress.cfg" . --tag oedb:latest
+  docker tag oedb:latest kherring/oedb:latest
+  # rm secrets/progress.cfg
+}
 
-# rm secrets/progress.cfg
-
-echo "docker build successful!"
-
-exit 0
+main_block
