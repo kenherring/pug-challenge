e4d6ae8dc5fcdbd0e1ccbabe5ad991c03b6034fe
kenherring <kennethherring@gmail.com>
2023-09-18 06:05:46 -0400

    scan code in SonarCloud

--- .docker/docker-build.sh
+++ .docker/docker-build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -eou pipefail
 
-#shellcheck disable=SC2034,SC2162
-read PROGRESS_CFG < /c/Progress/OpenEdge/progress.cfg
+if [ ! -f secrets/progress.cfg ]; then
+  PROGRESS_CFG=$DLC/progress.cfg
+  [ ! -f "$PROGRESS_CFG" ] && PROGRESS_CFG=/c/Progress/OpenEdge/progress.cfg
+  if [ ! -f "$PROGRESS_CFG" ]; then
+    echo "ERROR: cannot find progress.cfg"
+    exit 1
+  fi
+  mkdir -p secrets
+  cp "$PROGRESS_CFG" secrets/progress.cfg
+fi
 
 docker build -f .docker/Dockerfile --secret id=PROGRESS_CFG,src="$DLC/progress.cfg" . --tag oedb:latest
+docker tag oedb:latest kherring/oedb:latest
+
+# rm secrets/progress.cfg
 
 echo "docker build successful!"
 
