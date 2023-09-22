5e2e3d220b5019d179a056d35fc11ccec7e0c8f7
kenherring <kennethherring@gmail.com>
2023-09-22 09:35:02 -0400

    ablunit-in-docker.sh

---
+++ .docker/ablunit-in-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eou pipefail
+
+PROGRESS_CFG_BASE64=$(base64 "$DLC/progress.cfg")
+export PROGRESS_CFG_BASE64
+
+#shellcheck disable=SC2016
+COMMAND='base64 --decode <<< "$PROGRESS_CFG_BASE64" > "$DLC/progress.cfg" &&
+         ant compile test'
+
+PDIR="$(cmd //c cd || pwd)"
+
+docker container rm oedb-compile || true
+
+docker run -it --name oedb-compile \
+    -e PROGRESS_CFG_BASE64 \
+    -v "$PDIR":/home/pscadmin/ \
+    kherring/oedb:latest \
+    bash -c "$COMMAND"
